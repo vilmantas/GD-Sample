4e19d7a2ff13250cdc1bb9e63680eefed3505091
Laurynas <laurynas@puskunigis.eu>
2023-09-24 10:21:23 +0300

    Destroy hurdle on hit

--- Scenes/Hurdle/Hurdle.cs
+++ Scenes/Hurdle/Hurdle.cs
@@ -9,21 +9,18 @@ public partial class Hurdle : Area3D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Print("Hurdle ready");
-		this.BodyEntered += (body =>
-		{
-			Player player = body as Player;
-			if (player != null)
-			{
-				player.DamagePlayer(this._hurdleDamage);
-			}
-		});
+		this.BodyEntered += this.BodyEnteredHandler;
+
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	private void BodyEnteredHandler(Node3D body)
 	{
-		
+		Player player = body as Player;
+		if (player != null)
+		{
+			player.DamagePlayer(this._hurdleDamage);
+		}
+		QueueFree();
 	}
 	
 }
